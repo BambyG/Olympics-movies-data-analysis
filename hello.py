0bdf2022fb47be72630b608e8be5ad521f78877f
Bamby <bamby.gassama@gmail.com>
2017-07-27 12:14:07 +0200

    edit run()

--- hello.py
+++ hello.py
@@ -451,6 +451,7 @@ def hello(name=None):
 	return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
+	from os import environ
     app.run(host='0.0.0.0', debug=False, port=environ.get("PORT", 5000))
 
 
